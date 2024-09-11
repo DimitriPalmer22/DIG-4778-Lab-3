cb2797a7bf882d28ed794058279f20e935b1d18e
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-09-11 14:41:39 -0400

    Update EnemyScript.cs

--- Assets/_Scripts/EnemyScript.cs
+++ Assets/_Scripts/EnemyScript.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyScript : MonoBehaviour
 {
     public float speed = 1;
 
     private GameObject _target;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         // Find the target object
         _target = GameObject.Find("Player");
+
+        // Randomize the speed
+        speed = Random.Range(5, 10);
+        if (Random.Range(0, 1000) < 500)
+        {
+            Debug.Log($"Flipped Random");
+            speed *= -1;
+        }
+        
+        // Randomize the sprite color
+        GetComponent<SpriteRenderer>().color = new Color(Random.value, Random.value, Random.value);
     }
 
     // Update is called once per frame
@@ -20,5 +33,18 @@ public class EnemyScript : MonoBehaviour
     {
         // Look at the target
         transform.up = (_target.transform.position - transform.position);
+
+        transform.position += transform.right * (speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_target == null)
+            return;
+
+        // Set the color
+        // Draw a line from the enemy to the target
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, _target.transform.position);
     }
 }
